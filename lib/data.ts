9218c60b1f5e06df16de5f23c1af7c31de3d05e2
Siddharth Subramanian <sidsbrmnn@gmail.com>
2025-02-13 00:58:33 -0700

    cleanup: clean home page components

---
+++ lib/data.ts
@@ -0,0 +1,19 @@
+import { GitHub, LinkedIn, Twitter } from '../app/components/icons'
+
+export const socials = [
+  {
+    name: 'Siddharth on Twitter',
+    href: 'https://twitter.com/sidsbrmnn',
+    icon: Twitter,
+  },
+  {
+    name: 'Siddharth on GitHub',
+    href: 'https://github.com/sidsbrmnn',
+    icon: GitHub,
+  },
+  {
+    name: 'Siddharth on LinkedIn',
+    href: 'https://linkedin.com/in/sidsbrmnn',
+    icon: LinkedIn,
+  },
+]
